59e9a6d6ec2b52befd745712fb2a478dc7eae624
jethro894 <jethro@live.cn>
2014-09-20 12:36:45 -0700

    ajs library

--- WebContent/namesController.js
+++ WebContent/namesController.js
@@ -8,3 +8,8 @@ function namesController($scope) {
         {name:'Kai',country:'Denmark'}
     ];
 }
+
+function customersController($scope,$http) {
+    $http.get("http://www.w3schools.com/website/Customers_JSON.php")
+        .success(function(response) {$scope.names = response;});
+}
