20a24b172c9a9f0d24ea38dd56306a53239c2d05
jethro894 <jethro@live.cn>
2014-09-20 13:04:03 -0700

    separate app/controller

--- WebContent/myController.js
+++ WebContent/myController.js
@@ -1,15 +1,15 @@
 /**
  * Created by jethro on 9/20/14.
  */
-function namesController($scope) {
+app.controller("namesController", function($scope){
     $scope.names = [
         {name:'Jani',country:'Norway'},
         {name:'Hege',country:'Sweden'},
         {name:'Kai',country:'Denmark'}
     ];
-}
+});
 
-function customersController($scope,$http) {
+app.controller("customersController", function($scope,$http){
     $http.get("http://www.w3schools.com/website/Customers_JSON.php")
         .success(function(response) {$scope.names = response;});
-}
+});
