b7b1d242a6ab59c3580273069dfdfa6a40f4f6d5
tentacle <jethro@live.cn>
2014-09-23 15:49:45 -0700

    customer page

--- WebContent/myController.js
+++ WebContent/myController.js
@@ -13,3 +13,26 @@ app.controller("customersController", function($scope,$http){
     $http.get("http://www.w3schools.com/website/Customers_JSON.php")
         .success(function(response) {$scope.names = response;});
 });
+
+app.controller("searcherController", function($scope,$http){
+    $scope.test = false;
+    $scope.searchComplete = false;
+    $scope.updateShow= false;
+    $scope.userIDToBeUpdated = "";
+    $scope.searchUser = function(user_id){
+        $scope.userID = user_id;
+        $http.get("http://www.w3schools.com/website/Customers_JSON.php")
+            .success(function(response) {$scope.users = response;});
+        $scope.searchComplete = true;
+    };
+    $scope.updateWanted = function(user_id){
+        $scope.updateShow= true;
+        $scope.userIDToBeUpdated = user_id;
+
+    };
+    $scope.updateUser = function(user_id, attributes){
+        $scope.newName = attributes.Name;
+        $scope.newCountry = attributes.Country;
+        $scope.test = true;
+    };
+});
